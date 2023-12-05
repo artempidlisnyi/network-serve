d56948e23d70b04c6dbb8477731b03e545964b06
Artem <artem2005pidli@gmail.com>
2023-12-05 22:12:09 +0200

    feat: add parameter watch

--- network-serve.cpp
+++ network-serve.cpp
@@ -2,47 +2,45 @@
 #include <winsock2.h>
 #include <Windows.h>
 #include <iphlpapi.h>
-#include <winsock2.h>
+#include <cstring>
 
 #pragma comment(lib, "iphlpapi.lib")
 #pragma comment(lib, "ws2_32.lib")
 
-int main() {
-    // Code to retrieve TCP connections
-
+void printTcpConnections() {
     PMIB_TCPTABLE_OWNER_PID pTcpTable;
     DWORD dwSize = 0;
     DWORD dwRetVal = 0;
 
-    // Initialize Winsock
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cout << "WSAStartup failed." << std::endl;
-        return 1;
-    }
-
     // Retrieve table size
     if (GetExtendedTcpTable(NULL, &dwSize, FALSE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0) != ERROR_INSUFFICIENT_BUFFER) {
         std::cout << "GetExtendedTcpTable failed to get size." << std::endl;
-        WSACleanup();
-        return 1;
+        return;
     }
 
     // Allocate memory for the table
     pTcpTable = (PMIB_TCPTABLE_OWNER_PID)malloc(dwSize);
     if (pTcpTable == NULL) {
         std::cout << "Memory allocation failed." << std::endl;
-        WSACleanup();
-        return 1;
+        return;
     }
 
     // Retrieve TCP table
     if ((dwRetVal = GetExtendedTcpTable(pTcpTable, &dwSize, FALSE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0)) != NO_ERROR) {
         std::cout << "GetExtendedTcpTable failed with error: " << dwRetVal << std::endl;
         free(pTcpTable);
-        WSACleanup();
-        return 1;
+        return;
     }
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD cursorPosition = {0, 0};
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    DWORD written;
+
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    FillConsoleOutputCharacter(hConsole, ' ', consoleInfo.dwSize.X * consoleInfo.dwSize.Y, cursorPosition, &written);
+    FillConsoleOutputAttribute(hConsole, consoleInfo.wAttributes, consoleInfo.dwSize.X * consoleInfo.dwSize.Y, cursorPosition, &written);
+    SetConsoleCursorPosition(hConsole, cursorPosition);
 
     std::cout << "PID\tLocal Address\t\tPort\tRemote Address\t\tRemote Port" << std::endl;
     std::cout << "-------------------------------------------------------------------" << std::endl;
@@ -63,6 +61,36 @@ int main() {
     }
 
     free(pTcpTable);
-    WSACleanup();
+}
+
+int main(int argc, char *argv[]) {
+    // Initialize Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cout << "WSAStartup failed." << std::endl;
+        return 1;
+    }
+
+    bool watch = false; // Default refresh rate in milliseconds
+
+    // Check if -w option was passed and store the value in 'watch' variable
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-w") == 0) {
+            watch = true; // Convert the next argument to an integer (milliseconds)
+            break;
+        }
+    }
+    bool show = true;
+      std::cout << watch << std::endl;
+
+    while (show) {
+        printTcpConnections();
+        if (watch == false) {
+            show = false;
+        }
+        Sleep(2000); // Wait for 2 seconds and refresh output
+    }
+
+    WSACleanup(); // Cleanup Winsock
     return 0;
 }
